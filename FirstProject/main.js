e08f47db4f849209d3fd82c915aeb58aa32c6b2f
anotherparsa <anotherparsa@gmail.com>
2025-03-11 12:26:29 +0330

    get data from url

--- FirstProject/main.js
+++ FirstProject/main.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
+app.get('/product/:id', (req, res) =>{
+    res.send(req.params.id);
+})
+
 app.listen(8080, () =>{
     console.log("running server on port 8080");
 });
