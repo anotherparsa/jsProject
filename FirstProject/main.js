74d4b703248c64f8e0b74973025e9e267319054b
anotherparsa <anotherparsa@gmail.com>
2025-03-11 11:17:10 +0330

    sending response

--- FirstProject/main.js
+++ FirstProject/main.js
@@ -3,10 +3,11 @@ import * as secondary from './secondary.js'
 
 let server = http.createServer((req, res) => {
     console.log(req);
+    res.writeHead(200);
+    res.end("Hello World")
 })
 
-secondary.say_hello();
-
 server.listen(8080, ()=>{
+    secondary.say_hello();
     console.log("Server is running on port 8080");
 })
