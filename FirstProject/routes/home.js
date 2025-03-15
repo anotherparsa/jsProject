00f9ec9210590ef5932030bc70075a471ee712eb
anotherparsa <anotherparsa@gmail.com>
2025-03-15 13:59:52 +0330

    sending html file as response

--- FirstProject/routes/home.js
+++ FirstProject/routes/home.js
@@ -1,13 +1,19 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 
 export const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 router.get('/product/:id', (req, res) =>{
     res.send(req.params.id);
 })
 
 router.get('/home', (req, res) => {
-    res.send("this is home page from home.js routes")
+    res.sendFile(path.join(__dirname, '../views' ,'index.html'));
 })
 
 router.get('/redirectme', (req, res) =>{
