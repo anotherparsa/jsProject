51c9986c023fad4e8ec1180409107c2976c63969
anotherparsa <anotherparsa@gmail.com>
2025-03-15 08:58:03 +0330

    using middlewares

--- FirstProject/main.js
+++ FirstProject/main.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(`request method: ${req.method}, request url: ${req.url}`);
+    next();
+})
+
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
@@ -10,6 +15,6 @@ app.get('/product/:id', (req, res) =>{
     res.send(req.params.id);
 })
 
-app.listen(8080, () =>{
+app.listen(8080, () => {
     console.log("running server on port 8080");
 });
